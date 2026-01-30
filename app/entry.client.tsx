41722ff8e25703f0528f40eabd4ec9eb879ccd23
Linus <linus@r13.de>
2026-01-30 20:10:41 +0100

    aaa

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,13 +3,16 @@ import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
+    // @ts-ignore
+    if (window.BASENAME && window.__reactRouterContext) {
+        // @ts-ignore
+        window.__reactRouterContext.basename = window.BASENAME;
+    }
+
     hydrateRoot(
         document,
         <StrictMode>
-            <HydratedRouter
-                // @ts-ignore
-                basename={window.BASENAME}
-            />
+            <HydratedRouter />
         </StrictMode>
     );
 });
